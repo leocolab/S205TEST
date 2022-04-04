f47895de481ff5ed2c36e45b4a10dcfbada4695e
leocolab <leo.colabrese@yahoo.com>
2022-04-03 22:52:28 -0400

    Update application.py

--- application.py
+++ application.py
@@ -1,5 +1,9 @@
-from flask import Flask
+from flask import Flask, request, jsonify, redirect, url_for
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 import random
+import string
 app = Flask(__name__)
 
 @app.route("/")
